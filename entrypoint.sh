477c7e6f1e3b589ba7fd7ee544003aeb2b1af500
ShayCodes-dotcom <81831817+ShayCodes-dotcom@users.noreply.github.com>
2026-05-24 22:52:05 +0000

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,16 @@
 
 echo "==================================="
 
-git --global user.name "${GITHUB_ACTOR}"
-git --global user.email "${INPUT_EMAIL}"
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
+git remote set-url origin "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
 git add -A && git commit -m "Update Feed"
 
-git push --set-upstream origin master
+git push --set-upstream origin ${GITHUB_REF_NAME}
 
 echo "==================================="
